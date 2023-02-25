ecab7d0a9c6e7735a234fe4dabb9c6919e5c5f2a
piauming <piauming>
2023-02-25 17:02:10 +0800

    Edited deploy.js to deploy MyNFT.sol smart contract

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -11,15 +11,15 @@ async function main() {
   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
   const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const MyNFT = await hre.ethers.getContractFactory("MyNFT");
+  const myNFT = await MyNFT.deploy();
 
-  await lock.deployed();
+  await myNFT.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `MyNFT is deployed to ${myNFT.address}`
   );
 }
 
